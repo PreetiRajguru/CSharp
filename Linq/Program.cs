5fd4c79eb16a4a4fe4f45afb5a4a64493245df24
PreetiRajguru <rajgurupreeti0@gmail.com>
2023-03-15 11:48:51 +0530

    linq query and method implementation

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -8,6 +8,7 @@ namespace Linq
     {
         static void Main(string[] args)
         {
+            //using query implementation of linq
             var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             //query the list of numbers to get only the even numbers
@@ -29,6 +30,31 @@ namespace Linq
 
             //display the sum of the odd numbers
             Console.WriteLine("Sum of odd numbers: {0}", sumOfOddNumbers);
+
+
+
+            //using method implementation of linq
+            List<int> numbersList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var evenNumbersFound = numbersList.Where(n => n % 2 == 0);
+            var doubledNumbers = numbersList.Select(n => n * 2);
+            var sortedNumbers = numbersList.OrderBy(n => n);
+
+            Console.WriteLine("Even Numbers : ");
+            foreach (var n in evenNumbersFound)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("Doubled Numbers : ");
+            foreach (var n in doubledNumbers)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("Sorted Numbers : ");
+            foreach (var n in sortedNumbers)
+            {
+                Console.WriteLine(n);
+            }
+
             Console.ReadKey();
         }
     }
